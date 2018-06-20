54ed241b34d94782d185629e18070ce0d2675c83
Carlos Castillo <ccastillo.consultant@dot.nyc.gov>
2018-06-20 18:04:21 -0400

    - JWT Devise configured - User model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users,
+             path: '',
+             path_names: {
+                 sign_in: 'login',
+                 sign_out: 'logout',
+                 registration: 'signup'
+             },
+             controllers: {
+                 sessions: 'sessions',
+                 registrations: 'registrations'
+             }
 end
